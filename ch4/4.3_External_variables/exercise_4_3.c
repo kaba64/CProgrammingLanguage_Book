c4616b15a114daf9072dfd3ce0d84f48f9f471e9
kaba64 <Kazem Bazesefidpar>
2023-10-05 17:49:31 +0200

    Exercise 4.3 is done!

---
+++ ch4/4.3_External_variables/exercise_4_3.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+#define MAXOP 100    /*maximum size of operand or operator*/
+#define NUMBER '0'   /*signal that a number is found*/
+#define BUFSIZE 100
+#define MAXVAL 100   /*maximum depth of val stack*/
+int sp = 0;          /*next free stack position*/
+double val[MAXVAL];  /*value stack*/
+char buf[BUFSIZE];   /*buffer for ungetch*/
+int bufp =0;         /*next free position in buf*/
+
+int getop(char* s);
+int getch(void);
+void ungetch(int c);
+void push(double f);
+double pop(void);
+
+int main(int argc, char *argv[]){
+  int type;
+  double op2, op3;
+  char s[MAXOP];
+  
+  while((type=getop(s)) != EOF){
+    switch(type){
+    case NUMBER:
+      push(atof(s));
+      break;
+    case '+':
+      push(pop() + pop());
+      break;
+    case '*':
+      push(pop() * pop());
+      break;
+    case '-':
+      op2 = pop();
+      push(pop() - op2);
+      break;
+    case '/':
+      op2 = pop();
+      if(op2!=0.0)
+	push(pop() / op2);
+      else
+      	printf("error: zero divisor\n");
+      break;
+    case '%':
+      op2 = pop();
+      op3 = pop();
+      if(op3>=0.0 && op2>=0.0)
+	push((int)op3%(int)op2);
+      break;
+    }
+  }
+  printf("The operation result:  %lf\n",val[0]);
+  return 0;
+}
+
+int getop(char s[]){
+  int i,c;
+  i=0;
+  while((s[0]=c=getch())==' '|| c=='\t')
+    ;
+  s[1]='\0';
+  if(!isdigit(c) && c!='.'){
+    return c; /*not a number*/
+  }
+  if(isdigit(c)) /*collect integer parts*/
+    while(isdigit(s[++i]=c=getch()))
+      ;
+  if(c=='.')
+    while(isdigit(s[++i]=c=getch()))
+      ;
+  s[i]='\0';
+  if(c!= EOF)
+    ungetch(c);
+  return NUMBER; 
+}
+
+int getch(void) /*get a possibly pushed back character*/
+{
+  return (bufp>0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) /*push character back on input*/
+{
+  if(bufp>=BUFSIZE)
+    printf("ungetch: too many characters\n");
+  else
+    buf[bufp++]=c;
+}
+void push(double f) /*push f onto value stack*/
+{
+  if(sp<MAXVAL)
+    val[sp++]=f;
+  else
+    printf("error: stack full, cannot push %g\n",f);
+}
+ 
+double pop(void)
+{
+  if(sp>0)
+    return(val[--sp]);
+  else{
+    printf("error: stack empy\n");
+    return 0.0;
+  }
+}
